d136a8171e67efa48c124a5c3dda68c544255715
Adrien Leib <adrienleib@MacBook-Pro-de-Adrien.local>
2022-02-24 16:59:57 +0100

    first CSVTEST

---
+++ src/dao/CsvDao.java
@@ -0,0 +1,24 @@
+package dao;
+
+import java.util.List;
+
+import model.RaceTrack;
+
+public class CsvDao {
+
+	public boolean pathExist(String path, String file) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public List<String> getDataOnCsv(String path, String file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean addAllData(RaceTrack raceTrack, String path, String file) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ test/daoTest/CsvDaoTest.java
@@ -0,0 +1,105 @@
+package daoTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import dao.CsvDao;
+import model.Data;
+
+class csvDaoTest {
+
+	CsvDao csvDao = new CsvDao();
+
+	@Test
+	public void findcsvWithPathAndFile() {
+		// given
+		String path = "../ressource";
+		String file = "bdd.csv";
+
+		// when
+		assertTrue(csvDao.pathExist(path, file));
+
+		// then
+
+	}
+
+	@Test
+	public void findcsvWithNullPathAndFile() {
+
+		String path = null;
+		String file = "bdd.csv";
+
+		// when
+		assertFalse(csvDao.pathExist(path, file));
+
+		// then
+
+	}
+
+	@Test
+	public void findcsvWithPathAndNullFile() {
+
+		String path = "../ressource";
+		String file = null;
+
+		// when
+		assertFalse(csvDao.pathExist(path, file));
+
+		// then
+
+	}
+
+	@Test
+	public void readCsv() {
+
+		String path = "../ressource";
+		String file = null;
+
+		// when
+		assertNotNull(csvDao.getDataOnCsv(path, file));
+
+	}
+
+	@Test
+	public void readcsvWhenNullData() {
+
+		String path = "../ressource";
+		String file = null;
+
+		// when
+		assertNotNull(csvDao.getDataOnCsvs(path, file));
+
+	}
+
+	@Test
+	public void addAllDataOnCsv() {
+
+		// given
+		String path = "../ressource";
+		String file = "bdd_test.csv";
+
+		// RaceTrack t = new RaceTrack();
+		// Data.getRaceTrack().add(t);
+
+		assertTrue(csvDao.addAllData(Data.getRaceTrack(), path, file));
+
+	}
+
+	@Test
+	public void addAllNullDataOnCsv() {
+
+		// given
+		String path = "../ressource";
+		String file = "bdd_test.csv";
+
+		// RaceTrack t = new RaceTrack();
+		// Data.getRaceTrack().add(t);
+
+		assertTrue(csvDao.addAllData(null, path, file));
+
+	}
+
+}
