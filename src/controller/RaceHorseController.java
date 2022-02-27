faec9e677652a1684d1cf81c8a535291f874acc7
hugoj78 <hugoj.egu@gmail.com>
2022-02-27 14:42:19 +0100

    add Race Horse Controller Test

--- src/controller/RaceHorseController.java
+++ src/controller/RaceHorseController.java
@@ -1,5 +1,23 @@
 package controller;
 
+import dao.RaceHorseDao;
+import model.RaceHorse;
+
 public class RaceHorseController {
+
+	RaceHorseDao raceHorseDao = new RaceHorseDao();
+
+	public void createRaceHorse(String nameHorse) {
+		// raceHorseDao.createRaceHorse(nameHorse);
+	}
+
+	public void updateRaceHorseName(String newNameHorse) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean deleteRaceHorse(RaceHorse raceHorse) {
+		return false;
+	}
 
 }
--- test/controllerTest/RaceHorseControllerTest.java
+++ test/controllerTest/RaceHorseControllerTest.java
@@ -1,14 +1,84 @@
 package controllerTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import controller.RaceHorseController;
+import controller.RaceTrackController;
+import model.Data;
+import model.RaceHorse;
 
 class RaceHorseControllerTest {
 
+	RaceTrackController raceTrackController;
+	RaceHorseController raceHorseController;
+
+	@BeforeEach
+	void init() {
+		raceTrackController = new RaceTrackController();
+		raceHorseController = new RaceHorseController();
+	}
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void createRaceHorseTestOK() {
+		// GIVEN
+		String nameHorse = "Pablito";
+
+		// WHEN
+		raceHorseController.createRaceHorse(nameHorse);
+
+		// THEN
+		int nbrRaceHorse = Data.getRaceTrack().raceHorses.size();
+		assertEquals(1, nbrRaceHorse);
+		assertEquals(nameHorse, Data.getRaceTrack().raceHorses.get(nbrRaceHorse - 1).name);
+	}
+
+	@Test
+	public void updateNameRaceHorseTestOK() {
+		// GIVEN
+		String newNameHorse = "Pablito";
+		Data.getRaceTrack().raceHorses.add(new RaceHorse("ImNotPablito"));
+
+		// WHEN
+		raceHorseController.updateRaceHorseName(newNameHorse);
+
+		// THEN
+		int nbrRaceHorse = Data.getRaceTrack().raceHorses.size();
+		assertEquals(1, nbrRaceHorse);
+		assertEquals(newNameHorse, Data.getRaceTrack().raceHorses.get(nbrRaceHorse - 1).name);
+	}
+
+	@Test
+	public void deleteRaceHorseTestOK() {
+		// GIVEN
+		RaceHorse raceHorse = new RaceHorse("Chupapi");
+		Data.getRaceTrack().raceHorses.add(raceHorse);
+
+		// WHEN
+		boolean result = raceHorseController.deleteRaceHorse(raceHorse);
+
+		// THEN
+		int nbrRaceHorse = Data.getRaceTrack().raceHorses.size();
+		assertEquals(0, nbrRaceHorse);
+		assertTrue(result);
+	}
+
+	@Test
+	public void deleteRaceHorseNonExistantTestKO() {
+		// GIVEN
+		RaceHorse raceHorse = new RaceHorse("Chupapi");
+
+		// WHEN
+		boolean result = raceHorseController.deleteRaceHorse(raceHorse);
+
+		// THEN
+		int nbrRaceHorse = Data.getRaceTrack().raceHorses.size();
+		assertEquals(0, nbrRaceHorse);
+		assertFalse(result);
 	}
 
 }
