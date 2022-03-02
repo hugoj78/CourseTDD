54fabe1d72c84e839c3b4f72023d85c84689898b
Adrien Leib <adrienleib@MacBook-Pro-de-Adrien.local>
2022-03-02 23:11:43 +0100

    I see the bout of tunnel

--- src/controller/CsvController.java
+++ src/controller/CsvController.java
@@ -14,51 +14,49 @@ import model.RaceHorse;
 
 public class CsvController {
 
-	public void getDataTest() {
-
-	}
-
 	CsvDao csvDao = new CsvDao();
 
-	public ArrayList<RaceCircuit> getLastTenDataRaceCircuit(Files file) {
-		// TODO Auto-generated method stub
+	public ArrayList<RaceCircuit> getDataRaceCircuit(Files file) {
+
 		SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 		List<List<String>> circuit = csvDao.getDataOnCsv(file.path, file.name, file.extention);
-		System.out.println(circuit);
 		circuit.remove(0);
-		System.out.println(circuit);
-
-		Date d = new Date();
-
-		System.out.println(d);
-
 		ArrayList<RaceCircuit> arrayCircuit = new ArrayList<RaceCircuit>();
 
 		for (int i = 0; i < circuit.size(); i++) {
-			String name = circuit.get(i).get(0).toString();
 
-			Date date = null;
 			try {
-				System.out.println(circuit.get(i).get(1));
-				date = formatDate.parse(circuit.get(i).get(1).replaceAll("Z$", "+0000"));
-
-				// System.out.println(formatter.format(date));
+				String name = circuit.get(i).get(0).toString();
+				Date date = formatDate.parse(circuit.get(i).get(1).replaceAll("Z$", "+0000"));
+				RaceHorse besthorse = new RaceHorse(circuit.get(i).get(2));
+				arrayCircuit.add(new RaceCircuit(name, date, besthorse));
 
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			RaceHorse besthorse = new RaceHorse(circuit.get(i).get(2));
 
-			arrayCircuit.add(new RaceCircuit(name, date, besthorse));
 		}
 
-		System.out.println(arrayCircuit);
-		Collections.sort(arrayCircuit);
-
-		System.out.println(arrayCircuit);
-
 		return arrayCircuit;
+
+	}
+
+	public ArrayList<RaceCircuit> getTenLastCircuit(ArrayList<RaceCircuit> arrayCircuit) {
+		// TODO Auto-generated method stub
+		Collections.sort(arrayCircuit);
+		ArrayList<RaceCircuit> tenCircuit = new ArrayList<RaceCircuit>();
+
+		int i = arrayCircuit.size();
+		int e = 0;
+		while ((i > 0) && (e < 10)) {
+			tenCircuit.add(arrayCircuit.get(i - 1));
+
+			i--;
+			e++;
+		}
+		return tenCircuit;
+
 	}
 
 }
--- test/controllerTest/CsvControllerTest.java
+++ test/controllerTest/CsvControllerTest.java
@@ -1,47 +1,152 @@
 package controllerTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 import org.junit.jupiter.api.Test;
 
 import controller.CsvController;
 import model.Files;
+import model.RaceCircuit;
+import model.RaceHorse;
 
 class CsvControllerTest {
 
 	CsvController csvController = new CsvController();
 
 	@Test
-	public void getLastTenDataRaceCircuitTest() {
+	public void getDataRaceCircuitTest() {
 		String path = "./src/ressources";
 		String file = "circuits";
 		String extention = "csv";
 		Files f = new Files(file, path, extention);
 
-		System.out.println(1);
-
-		assertNotNull(csvController.getLastTenDataRaceCircuit(f));
+		assertNotNull(csvController.getDataRaceCircuit(f));
 
 	}
 
 	@Test
-	public void getLastTenDataRaceCircuitWHenFileNullTest() {
-		String path = "./src/ressources";
-		String file = null;
-		String extention = "csv";
-		Files f = new Files(file, path, extention);
+	public void getLastTenDataRaceCircuitSizeTest() {
+		RaceHorse h1 = new RaceHorse("Toto");
+		Date d1 = new Date();
+		RaceCircuit c1 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c2 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c3 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c4 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c5 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c6 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c7 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c8 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c9 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c10 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c11 = new RaceCircuit("ImNotPablito", d1, h1);
 
-		assertNotNull(csvController.getLastTenDataRaceCircuit(f));
+		ArrayList<RaceCircuit> arrayCircuit = new ArrayList<RaceCircuit>();
+		arrayCircuit.add(c1);
+		arrayCircuit.add(c2);
+		arrayCircuit.add(c3);
+		arrayCircuit.add(c4);
+		arrayCircuit.add(c5);
+		arrayCircuit.add(c6);
+		arrayCircuit.add(c7);
+		arrayCircuit.add(c8);
+		arrayCircuit.add(c9);
+		arrayCircuit.add(c10);
+		arrayCircuit.add(c11);
+
+		ArrayList<RaceCircuit> tenLastCircuit = csvController.getTenLastCircuit(arrayCircuit);
+
+		assertEquals(10, tenLastCircuit.size());
+
 	}
 
 	@Test
-	public void getLastTenDataRaceCircuitWHenDateNotInFirstLineTest() {
-		String path = "./src/ressources";
-		String file = null;
-		String extention = "csv";
-		Files f = new Files(file, path, extention);
+	public void getLastTenDataRaceCircuitSizWhenRaceCircuitHasNotTenRaceTest() {
+		RaceHorse h1 = new RaceHorse("Toto");
+		Date d1 = new Date();
+		RaceCircuit c1 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c2 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c3 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c4 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c5 = new RaceCircuit("ImNotPablito", d1, h1);
 
-		assertNotNull(csvController.getLastTenDataRaceCircuit(f));
+		ArrayList<RaceCircuit> arrayCircuit = new ArrayList<RaceCircuit>();
+		arrayCircuit.add(c1);
+		arrayCircuit.add(c2);
+		arrayCircuit.add(c3);
+		arrayCircuit.add(c4);
+		arrayCircuit.add(c5);
+
+		ArrayList<RaceCircuit> tenLastCircuit = csvController.getTenLastCircuit(arrayCircuit);
+
+		assertNotEquals(10, tenLastCircuit.size());
+
+	}
+
+	@Test
+	public void getTenLastDataRaceCircuitLogicalTest() {
+		RaceHorse h1 = new RaceHorse("Toto");
+		Date d1 = new Date();
+		Date d2 = new Date();
+		Date d3 = new Date();
+		Date d4 = new Date();
+		Date d5 = new Date();
+		Date d6 = new Date();
+		Date d7 = new Date();
+		Date d8 = new Date();
+		Date d9 = new Date();
+		Date d10 = new Date();
+		Date d11 = new Date();
+
+		RaceCircuit c1 = new RaceCircuit("ImNotPablito", d1, h1);
+		RaceCircuit c2 = new RaceCircuit("ImNotPablito", d2, h1);
+		RaceCircuit c3 = new RaceCircuit("ImNotPablito", d3, h1);
+		RaceCircuit c4 = new RaceCircuit("ImNotPablito", d4, h1);
+		RaceCircuit c5 = new RaceCircuit("ImNotPablito", d5, h1);
+		RaceCircuit c6 = new RaceCircuit("ImNotPablito", d6, h1);
+		RaceCircuit c7 = new RaceCircuit("ImNotPablito", d7, h1);
+		RaceCircuit c8 = new RaceCircuit("ImNotPablito", d8, h1);
+		RaceCircuit c9 = new RaceCircuit("ImNotPablito", d9, h1);
+		RaceCircuit c10 = new RaceCircuit("ImNotPablito", d10, h1);
+		RaceCircuit c11 = new RaceCircuit("ImNotPablito", d11, h1);
+
+		ArrayList<RaceCircuit> arrayCircuit = new ArrayList<RaceCircuit>();
+		arrayCircuit.add(c11);
+		arrayCircuit.add(c2);
+		arrayCircuit.add(c3);
+		arrayCircuit.add(c4);
+		arrayCircuit.add(c5);
+		arrayCircuit.add(c10);
+		arrayCircuit.add(c7);
+		arrayCircuit.add(c8);
+		arrayCircuit.add(c9);
+		arrayCircuit.add(c8);
+		arrayCircuit.add(c11);
+
+		ArrayList<RaceCircuit> trueTenLastArrayCircuit = new ArrayList<RaceCircuit>();
+		trueTenLastArrayCircuit.add(c1);
+		trueTenLastArrayCircuit.add(c2);
+		trueTenLastArrayCircuit.add(c3);
+		trueTenLastArrayCircuit.add(c4);
+		trueTenLastArrayCircuit.add(c5);
+		trueTenLastArrayCircuit.add(c6);
+		trueTenLastArrayCircuit.add(c7);
+		trueTenLastArrayCircuit.add(c8);
+		trueTenLastArrayCircuit.add(c9);
+		trueTenLastArrayCircuit.add(c10);
+
+		ArrayList<RaceCircuit> tenLastCircuit = csvController.getTenLastCircuit(arrayCircuit);
+
+		System.out.println(trueTenLastArrayCircuit);
+
+		System.out.println("------");
+
+		System.out.println(tenLastCircuit);
+		assertEquals(trueTenLastArrayCircuit, tenLastCircuit);
 
 	}
 
